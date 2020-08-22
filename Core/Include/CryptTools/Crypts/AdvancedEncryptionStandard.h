1769c9ad1c91d7736874f471b161df160c2939fa
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-08-22 11:55:54 +0900

    クラスを新規作成

---
+++ Core/Include/CryptTools/Crypts/AdvancedEncryptionStandard.h
@@ -0,0 +1,114 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---   The Cryption Library and Tools   ---              **
+**                                                                      **
+**          Copyright (C), 2020-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of AdvancedEncryptionStandard class.
+**
+**      @file       Crypts/AdvancedEncryptionStandard.h
+**/
+
+#if !defined( CRYPTTOOLS_COMMON_INCLUDED_ADVANCED_ENCRYPTION_STANDARD_H )
+#    define   CRYPTTOOLS_COMMON_INCLUDED_ADVANCED_ENCRYPTION_STANDARD_H
+
+#include    "CryptTools/Common/CryptToolsSettings.h"
+
+CRYPTTOOLS_NAMESPACE_BEGIN
+namespace  Crypts  {
+
+//========================================================================
+//
+//    AdvancedEncryptionStandard  class.
+//
+
+class  AdvancedEncryptionStandard
+{
+
+//========================================================================
+//
+//    Internal Type Definitions.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+public:
+
+    //----------------------------------------------------------------
+    /**   インスタンスを初期化する
+    **  （デフォルトコンストラクタ）。
+    **
+    **/
+    AdvancedEncryptionStandard();
+
+    //----------------------------------------------------------------
+    /**   インスタンスを破棄する
+    **  （デストラクタ）。
+    **
+    **/
+    virtual  ~AdvancedEncryptionStandard();
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+//========================================================================
+//
+//    Other Features.
+//
+public:
+    //  テストクラス。  //
+    friend  class   AdvancedEncryptionStandardTest;
+};
+
+}   //  End of namespace  Crypts
+CRYPTTOOLS_NAMESPACE_END
+
+#endif
---
+++ Core/Lib/Crypts/AdvancedEncryptionStandard.cpp
@@ -0,0 +1,93 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---   The Cryption Library and Tools   ---              **
+**                                                                      **
+**          Copyright (C), 2020-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of AdvancedEncryptionStandard class.
+**
+**      @file       Crypts/AdvancedEncryptionStandard.cpp
+**/
+
+CRYPTTOOLS_NAMESPACE_BEGIN
+namespace  Crypts  {
+
+namespace  {
+
+}   //  End of (Unnamed) namespace.
+
+
+//========================================================================
+//
+//    AdvancedEncryptionStandard  class.
+//
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+AdvancedEncryptionStandard::AdvancedEncryptionStandard()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+AdvancedEncryptionStandard::~AdvancedEncryptionStandard()
+{
+}
+
+//========================================================================
+//
+//    Public Member Functions (Implement Pure Virtual).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Overrides).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Pure Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions (Virtual Functions).
+//
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+}   //  End of namespace  Crypts
+CRYPTTOOLS_NAMESPACE_END
