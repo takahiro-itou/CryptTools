5d5d8958e63bc5a9f7377b0ff63bb775ba8fc828
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-08-29 21:17:49 +0900

    ソースファイルを新規作成

---
+++ Core/Lib/Crypts/Tests/AesSteps05Test.cpp
@@ -0,0 +1,67 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---   The Cryption Library and Tools   ---              **
+**                                                                      **
+**          Copyright (C), 2020-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of Test Case 'AesSteps05'.
+**
+**      @file       Crypts/Tests/AesSteps05Test.cpp
+**/
+
+#include    "AdvancedEncryptionStandardTest.h"
+
+
+CRYPTTOOLS_NAMESPACE_BEGIN
+namespace  Crypts  {
+
+//========================================================================
+//
+//    AesSteps05Test  class.
+//
+/**
+**    クラス AdvancedEncryptionStandard の単体テスト。
+**/
+
+class  AesSteps05Test : public  AdvancedEncryptionStandardTest
+{
+    CPPUNIT_TEST_SUITE(AesSteps05Test);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+
+    virtual  void   setUp()     override    { }
+    virtual  void   tearDown()  override    { }
+
+private:
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( AesSteps05Test );
+
+//========================================================================
+//
+//    Tests.
+//
+
+}   //  End of namespace  Crypts
+CRYPTTOOLS_NAMESPACE_END
+
+//========================================================================
+//
+//    エントリポイント。
+//
+
+int  main(int argc, char * argv[])
+{
+    return ( executeCppUnitTests(argc, argv) );
+}
