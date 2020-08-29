87e79a849d7219878651ae253e4682041e302813
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-08-29 09:33:18 +0900

    ヘッダファイルを新規作成

---
+++ Core/Lib/Crypts/Tests/AdvancedEncryptionStandardTest.h
@@ -0,0 +1,30 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---   The Cryption Library and Tools   ---              **
+**                                                                      **
+**          Copyright (C), 2020-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Interface of Test Case 'AdvancedEncryptionStandard'.
+**
+**      @file       Crypts/Tests/AdvancedEncryptionStandardTest.h
+**/
+
+#if !defined( CRYPTTOOLS_CRYPTS_TESTS_INCLUDED_AES_TEST_H )
+#    define   CRYPTTOOLS_CRYPTS_TESTS_INCLUDED_AES_TEST_H
+
+CRYPTTOOLS_NAMESPACE_BEGIN
+namespace  Crypts  {
+
+}   //  End of namespace  Crypts
+CRYPTTOOLS_NAMESPACE_END
+
+#endif
