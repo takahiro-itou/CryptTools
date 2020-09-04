89d669696175aa398d844db3c38e5191fa3b7752
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-09-05 08:16:34 +0900

    ヘッダファイルを新規作成

---
+++ Core/Lib/Crypts/AesTables.h
@@ -0,0 +1,24 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**              ---   The Cryption Library and Tools   ---              **
+**                                                                      **
+**          Copyright (C), 2020-2020, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING and LICENSE files)                    **
+**          GNU General Public License (GPL) version 3,                 **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      An Implementation of AdvancedEncryptionStandard class.
+**
+**      @file       Crypts/AesTables.h
+**/
+
+#if !defined( CRYPTTOOLS_CRYPTS_INCLUDED_AES_TABLES_H )
+#    define   CRYPTTOOLS_CRYPTS_INCLUDED_AES_TABLES_H
+
+#endif
